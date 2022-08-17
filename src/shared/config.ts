aedb74bdb3938fe0e868d8c272f0b4b4a07c4092
Juan Reyes <u201620531@upc.edu.pe>
2022-08-17 08:20:57 -0500

    Add config and filters values

---
+++ src/shared/config.ts
@@ -0,0 +1,14 @@
+export const supportTables = {
+  documentType: 'TTD',
+  formatType: 'TFD',
+  transactionType: 'TDT',
+};
+
+export const filters = {
+  placeholders: {
+    documentType: 'Tipo de documento',
+    formatType: 'Tipo de formato',
+    transactionType: 'Tipo de transacción',
+    wayPay: 'Forma de pago',
+  },
+};
