e3538e44c9725a63284112f52e3d47278e3ccd2a
Juan Reyes <u201620531@upc.edu.pe>
2022-08-27 08:07:35 -0500

    Delete TransactionType Componemt and Service

--- src/app/interfaces/transaction-type.ts
+++
@@ -1,9 +0,0 @@
-export interface TransactionType {
-  id: string; //V-Venta, C-Compra, P-Personal
-  description: string;
-  abbreviation?: string;
-  type?: string[]; //R-Reporte
-  status?: string; //A-Activo, I-Inactivo
-  creationDate?: String;
-  creationUser?: string;
-}
