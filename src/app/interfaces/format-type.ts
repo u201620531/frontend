c76e63940149ec96eb2cee574c42cebce8c582ca
Juan Reyes <u201620531@upc.edu.pe>
2022-08-27 08:05:59 -0500

    Delete FormatTypeComponent and  FormatTypeService

--- src/app/interfaces/format-type.ts
+++
@@ -1,9 +0,0 @@
-export interface FormatType {
-  id: string;
-  description: string;
-  abbreviation?: string;
-  type?: string[]; //V-Venta, C-Compra, P-Personal, C-Contabilidad
-  status: string; //A-Activo, I-Inactivo
-  creationDate?: string;
-  creationUser?: string;
-}
