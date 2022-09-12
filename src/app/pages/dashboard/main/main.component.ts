98d2003d4869fd5bd4fd136b74eafe9795335f0a
Juan Reyes <u201620531@upc.edu.pe>
2022-09-12 13:28:15 -0500

    Modify mainComponent

--- src/app/pages/dashboard/main/main.component.ts
+++ src/app/pages/dashboard/main/main.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { EmpleadoService } from 'src/app/services/empleado.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
+  nombreUsuario = '';
+  constructor(
+    private _usuarioService: UsuarioService,
+    private _empleadoService: EmpleadoService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.consultaEmpleado();
   }
 
+  async consultaEmpleado() {
+    this._empleadoService
+      .listarEmpleadoPoridEmpleado(
+        this._usuarioService.currentUsuarioValue.idEmpleado
+      )
+      .subscribe((res: any) => {
+        this.nombreUsuario = res.nombre + " " + res.apellido;
+      });
+  }
 }
