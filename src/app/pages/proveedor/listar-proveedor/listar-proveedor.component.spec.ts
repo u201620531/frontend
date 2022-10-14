d38bf2bf7f3f1a3c97f7e0d8d7ec9d7e48b5f4bc
Juan Reyes <u201620531@upc.edu.pe>
2022-10-14 12:16:09 -0500

    Update unitTest

--- src/app/pages/proveedor/listar-proveedor/listar-proveedor.component.spec.ts
+++ src/app/pages/proveedor/listar-proveedor/listar-proveedor.component.spec.ts
@@ -8,14 +8,21 @@ import { MatTableModule } from '@angular/material/table';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ProveedorService } from 'src/app/services/proveedor.service';
+import { Proveedor } from 'src/app/interfaces/proveedor';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 describe('ListarProveedorComponent', () => {
   let component: ListarProveedorComponent;
   let fixture: ComponentFixture<ListarProveedorComponent>;
+  let _ProveedorService: ProveedorService;
+  let listaProveedor: Proveedor[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListarProveedorComponent ],
+      declarations: [ListarProveedorComponent],
       imports: [
         MatPaginatorModule,
         MatSnackBarModule,
@@ -24,6 +31,8 @@ describe('ListarProveedorComponent', () => {
         HttpClientTestingModule,
         FormsModule,
         ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatProgressSpinnerModule,
         RouterModule.forRoot([]),
       ],
     }).compileComponents();
@@ -32,10 +41,86 @@ describe('ListarProveedorComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarProveedorComponent);
     component = fixture.componentInstance;
+    _ProveedorService = fixture.debugElement.injector.get(ProveedorService);
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Crear Componemte', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método ngOnInit', () => {
+    spyOn(component, 'ngOnInit');
+
+    expect(component.placeholderValue).toBe('Proveedor');
+  });
+
+  it('Llama al método listarProveedor y retorna lista vacia', () => {
+    spyOn(_ProveedorService, 'listarPoveedores').and.returnValue(
+      of(listaProveedor)
+    );
+
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método listarProveedor y retorna lista', () => {
+    listaProveedor = [
+      {
+        idProveedor: '01',
+        razonSocial: 'TEST',
+        idTipoProveedor: 'J',
+        idTipoDocumento: 'FT',
+        email1: 'test@gmail.com',
+        email2: '',
+        nombreComercial: '',
+        nroDocumento: '111111111',
+        estado: 'A',
+        fechaCreacion: '',
+        usuarioCreacion: 'admin',
+      },
+    ];
+    spyOn(_ProveedorService, 'listarPoveedores').and.returnValue(
+      of(listaProveedor)
+    );
+
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método eliminarProveedor', () => {
+    const response = [
+      {
+        id: '01',
+        message: 'Factura',
+        detail: 'FAC',
+      },
+    ];
+    spyOn(_ProveedorService, 'eliminarProveedor').and.returnValue(of(response));
+
+    component.eliminarProveedor('');
+
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método eliminarProveedor y listarProveedor', () => {
+    const response = [
+      {
+        id: 1,
+        message: 'Factura',
+        detail: 'FAC',
+      },
+    ];
+    spyOn(_ProveedorService, 'eliminarProveedor').and.returnValue(of(response));
+    spyOn(_ProveedorService, 'listarPoveedores');
+
+    component.eliminarProveedor('');
+
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método modificarProveedor', () => {
+    component.modificarProveedor('', 1);
+
     expect(component).toBeTruthy();
   });
 });
--- src/app/pages/tipo-documento/listar-tipo-documento/listar-tipo-documento.component.spec.ts
+++ src/app/pages/tipo-documento/listar-tipo-documento/listar-tipo-documento.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 
 import { ListarTipoDocumentoComponent } from './listar-tipo-documento.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
@@ -8,14 +8,21 @@ import { MatTableModule } from '@angular/material/table';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TipoDocumentoService } from 'src/app/services/tipo-documento.service';
+import { TipoDocumento } from 'src/app/interfaces/tipo-documento';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 describe('ListarTipoDocumentoComponent', () => {
   let component: ListarTipoDocumentoComponent;
   let fixture: ComponentFixture<ListarTipoDocumentoComponent>;
+  let _TipoDocumentoService: TipoDocumentoService;
+  let listaTipoDocumento: TipoDocumento[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListarTipoDocumentoComponent ],
+      declarations: [ListarTipoDocumentoComponent],
       imports: [
         MatPaginatorModule,
         MatSnackBarModule,
@@ -24,6 +31,8 @@ describe('ListarTipoDocumentoComponent', () => {
         HttpClientTestingModule,
         FormsModule,
         ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatProgressSpinnerModule,
         RouterModule.forRoot([]),
       ],
     }).compileComponents();
@@ -32,10 +41,85 @@ describe('ListarTipoDocumentoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarTipoDocumentoComponent);
     component = fixture.componentInstance;
+    _TipoDocumentoService =
+      fixture.debugElement.injector.get(TipoDocumentoService);
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Crear Componemte', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método ngOnInit', () => {
+    spyOn(component, 'ngOnInit');
+
+    expect(component.placeholderValue).toBe('Tipo de documento');
+  });
+
+  it('Llama al método listarTipoDocumento y retorna lista vacia', () => {
+    spyOn(_TipoDocumentoService, 'listarTipoDocumento').and.returnValue(
+      of(listaTipoDocumento)
+    );
+
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método listarTipoDocumento y retorna lista', () => {
+    listaTipoDocumento = [
+      {
+        idTipoDocumento: '01',
+        descripcion: 'Factura',
+        abreviatura: 'FAC',
+        estado: 'A',
+        fechaCreacion: '',
+      },
+    ];
+    spyOn(_TipoDocumentoService, 'listarTipoDocumento').and.returnValue(
+      of(listaTipoDocumento)
+    );
+
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método eliminarTipoDocumento', () => {
+    const response = [
+      {
+        id: '01',
+        message: 'Factura',
+        detail: 'FAC',
+      },
+    ];
+    spyOn(_TipoDocumentoService, 'eliminarTipoDocumento').and.returnValue(
+      of(response)
+    );
+
+    component.eliminarTipoDocumento('');
+
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método eliminarTipoDocumento y listarTipoDocumento', () => {
+    const response = [
+      {
+        id: 1,
+        message: 'Factura',
+        detail: 'FAC',
+      },
+    ];
+    spyOn(_TipoDocumentoService, 'eliminarTipoDocumento').and.returnValue(
+      of(response)
+    );
+    spyOn(_TipoDocumentoService, 'listarTipoDocumento');
+
+    component.eliminarTipoDocumento('');
+
+    expect(component).toBeTruthy();
+  });
+
+  it('Llama al método modificarTipoDocumento', () => {
+    component.modificarTipoDocumento('', 1);
+
     expect(component).toBeTruthy();
   });
 });
